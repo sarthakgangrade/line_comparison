15d44fd683e90d0a59d8b817a3751d44f4de2517
“sarthak <sarthakgangrade0@gmail.com>
2021-10-28 07:21:31 +0530

    add UC1

--- Line_comparison/Program.cs
+++ Line_comparison/Program.cs
@@ -10,8 +10,28 @@ namespace Line_comparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+            double x1, x2, y1, y2;
+            double len, length;
+            double point1, point2;
+
+            Console.WriteLine("Enter value of x1:");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter value of y1:");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter value of x2:");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter value of y2");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            // formula of length = sqrt[(x2 -x1)^2 + (y2-y1)^2]
+            point1 = Math.Pow((x2 - x1), 2);
+            point2 = Math.Pow((y2 - y1), 2);
+            len = point1 + point2;
+            length = Math.Sqrt(len);
+
+            Console.WriteLine("The length of line is: " + length);
             Console.ReadLine();
+
         }
     }
 }
