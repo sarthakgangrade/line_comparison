27af5975cf34fac969773e0561450933a75976dc
“sarthak <sarthakgangrade0@gmail.com>
2021-10-28 07:10:18 +0530

    add line comparison on the main branch

---
+++ Line_comparison/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+            Console.ReadLine();
+        }
+    }
+}
